2ecfb02c1fcdbd154f3232196e3037b8488472d7
Anant Rawat <anant1995rawat@gmail.com>
2020-10-09 15:09:24 +0530

    Redux added in project

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from "react";
+import Navbar from "../src/components/layout/Navbar";
+import Landing from "../src/components/layout/Landing";
+import Login from "../src/components/auth/Login";
+import Register from "../src/components/auth/Register";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Alert from "./components/layout/Alert";
+//Redux
+import { Provider } from "react-redux";
+import store from "./store";
+import "./App.css";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;

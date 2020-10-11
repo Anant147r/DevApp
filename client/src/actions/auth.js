e1bc3f8d9dc1c581ef9be60d88eccf290b6a72e3
Anant Rawat <anant1995rawat@gmail.com>
2020-10-11 11:14:05 +0530

    Logout functionality added

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,6 +8,7 @@ import {
   AUTH_ERROR,
   LOGIN_FAIL,
   LOGIN_SUCCESS,
+  LOGOUT,
 } from "./types";
 
 //Load User
@@ -75,4 +76,9 @@ export const login = (email, password) => async (dispatch) => {
     }
     dispatch({ type: LOGIN_FAIL });
   }
+};
+
+//LOGOUT /Clear Profile
+export const logout = () => (dispatch) => {
+  dispatch({ type: LOGOUT });
 };

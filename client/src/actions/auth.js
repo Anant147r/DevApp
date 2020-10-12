84bf2045cbfdff7e1496be0c83eae487932ca5de
Anant Rawat <anant1995rawat@gmail.com>
2020-10-12 10:34:43 +0530

    Profiles access for valid user added

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -9,6 +9,7 @@ import {
   LOGIN_FAIL,
   LOGIN_SUCCESS,
   LOGOUT,
+  CLEAR_PROFILE,
 } from "./types";
 
 //Load User
@@ -80,5 +81,6 @@ export const login = (email, password) => async (dispatch) => {
 
 //LOGOUT /Clear Profile
 export const logout = () => (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
